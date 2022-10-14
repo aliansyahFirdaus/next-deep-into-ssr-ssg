23be9a192713a903a00c50fba886ae52409c1992
aliansyahFirdaus <aliansyah.it@gmail.com>
2022-10-14 17:46:14 +0700

    fix bug

--- pages/isr.js
+++ pages/isr.js
@@ -1,4 +1,4 @@
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const res = await fetch("https://flash-the-slow-api.herokuapp.com/delay/5000/url/https://634933eea59874146b19e6a3.mockapi.io/next/users")
   const data = await res.json()
 
